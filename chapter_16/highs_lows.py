55e09ea120fdc6f7fa67fd7f788364b465013143
Vinwan <wellber.wan@gmail.com>
2018-06-19 18:28:50 +0800

    add low temperatures

--- chapter_16/highs_lows.py
+++ chapter_16/highs_lows.py
@@ -8,7 +8,7 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
@@ -16,14 +16,16 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
-    print(highs)
+        low = int(row[3])
+        lows.append(low)
 
 # draw graph base on data
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
 
 # set graph format
-plt.title("Daily high temperatures - 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature(F)", fontsize=16)
