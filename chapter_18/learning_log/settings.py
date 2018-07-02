4f8b899d487f90e7fa1bad8ae46a1848893f86bd
Vinwan <wellber.wan@gmail.com>
2018-07-02 22:09:56 +0800

    add gunicorn

--- chapter_18/learning_log/settings.py
+++ chapter_18/learning_log/settings.py
@@ -135,3 +135,24 @@ LOGIN_URL = '/users/login/'
 BOOTSTRAP3 = {
     'include_jquery': True,
 }
+
+# Heroku settings
+
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+    }
+
+    # let request.is_secure agree X-Forwarded-Proto
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # allowed all hosts
+    ALLOWED_HOSTS = ['*']
+
+    # static resuorce configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
